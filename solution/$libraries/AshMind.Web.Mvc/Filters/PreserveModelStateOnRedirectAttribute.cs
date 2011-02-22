d2965ee4e8b8085b3cba183e5bafa6bab597a07d
ashmind <ashmind@gmail.com>
2011-02-22 15:55:54 +0300

    Added some common helpers.

---
+++ solution/$libraries/AshMind.Web.Mvc/Filters/PreserveModelStateOnRedirectAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AshMind.Web.Mvc.Filters {
+    public class PreserveModelStateOnRedirectAttribute : FilterAttribute, IActionFilter {
+        private const string TempDataKey = "PreserveModelStateOnRedirect.ModelState";
+
+        public void OnActionExecuting(ActionExecutingContext filterContext) {
+            if (!filterContext.Controller.TempData.ContainsKey(TempDataKey))
+                return;
+
+            var preservedModelState = (ModelStateDictionary)filterContext.Controller.TempData[TempDataKey];
+            var modelState = filterContext.Controller.ViewData.ModelState;
+            foreach (var pair in preservedModelState) {
+                modelState.Add(pair.Key, pair.Value);
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext) {
+            if (!(filterContext.Result is RedirectResult) && !(filterContext.Result is RedirectToRouteResult))
+                return;
+
+            var modelState = filterContext.Controller.ViewData.ModelState;
+            if (modelState.IsValid)
+                return;
+
+            var tempData = filterContext.Controller.TempData;
+            tempData[TempDataKey] = modelState;
+        }
+    }
+}
---
+++ solution/$libraries/AshMind.Web.Mvc/Filters/UseXHtmlWherePossibleAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AshMind.Web.Mvc.Filters {
+    public class UseXHtmlWherePossibleAttribute : FilterAttribute, IResultFilter {
+        public void OnResultExecuting(ResultExecutingContext filterContext) {
+            var response = filterContext.HttpContext.Response;
+            if (response.ContentType != "text/html")
+                return;
+
+            if (filterContext.HttpContext.Request.Browser.Type.ToUpper().Contains("IE"))
+                return;
+
+            response.ContentType = "application/xhtml+xml";
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) {
+        }
+    }
+}
